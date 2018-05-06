3589ce71bcc1a52c303d3541b57fb42fa0061c88
agustin moya <36174151+agustinmoyarancibia@users.noreply.github.com>
2018-05-06 20:09:37 -0300

    Add files via upload

---
+++ tp 2 haciendo/tp 2 haciendo/funciones.h
@@ -0,0 +1,38 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+int menu();
+
+typedef struct {
+
+    char nombre[50];
+    int edad;
+    int estado;
+    int dni;
+
+}EPersona;
+
+int buscarPorDni(EPersona lista[],int tam, int dni);
+void inicializadorPersonas( EPersona vec[], int tam );
+void altaEmpleado(EPersona lista[], int tam);
+void mostrarPersonas(EPersona lista[], int tam);
+void bajaPersona(EPersona lista[], int tam);
+void mostrarAterisco(EPersona lista[], int tam);
+void graficosEdades(EPersona lista[], int tam);
+
+/**
+ * Obtiene el primer indice libre del array.
+ * @param lista el array se pasa como parametro.
+ * @return el primer indice disponible
+ */
+int obtenerEspacioLibre(EPersona lista[], int tam);
+
+/**
+ * Obtiene el indice que coincide con el dni pasado por parametro.
+ * @param lista el array se pasa como parametro.
+ * @param dni el dni a ser buscado en el array.
+ * @return el indice en donde se encuentra el elemento que coincide con el parametro dni
+ */
+
+
+
+#endif // FUNCIONES_H_INCLUDED
---
+++ tp 2 haciendo/tp 2 haciendo/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+
+#define TAM 20
+
+
+
+
+int main()
+{
+    char seguir='s';
+    EPersona persona[TAM];
+    inicializadorPersonas(persona,TAM);
+
+    while(seguir=='s')
+    {
+        switch(menu())
+        {
+        case 1:
+            altaEmpleado(persona, TAM);
+            system("pause");
+            system("cls");
+            break;
+        case 2:
+            bajaPersona(persona, TAM);
+            system("pause");
+            system("cls");
+            break;
+        case 3:
+            mostrarPersonas(persona, TAM);
+            system("pause");
+            system("cls");
+            break;
+        case 4:
+            graficosEdades(persona, TAM);
+            system("pause");
+            break;
+        case 5:
+            seguir = 'n';
+            system("pause");
+            break;
+        default:
+            printf("elija opcion del 1 al 5\n");
+            system("pause");
+        }
+    }
+
+    return 0;
+}
+
